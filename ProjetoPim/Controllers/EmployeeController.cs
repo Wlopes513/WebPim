780309d9d537c77eab56c92b933c8205a1af565e
Wlopes <wlopes_junior2013@hotmail.com>
2023-11-05 12:07:46 -0300

    Implemented search in database

---
+++ ProjetoPim/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoPim.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoPim.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmplyeeController : ControllerBase
+    {
+        private readonly ILogger<EmplyeeController> _logger;
+
+        public EmplyeeController(ILogger<EmplyeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<EmployeeInfo> Get()
+        {
+            var rng = new Random();
+            return null;
+        }
+    }
+}
