65e4a22e0c779d43e137152a7828971ef387623c
Wlopes <wlopes_junior2013@hotmail.com>
2023-11-12 01:57:25 -0300

    Fixed login validation

--- ProjetoPim/ClientApp/src/components/Login.tsx
+++ ProjetoPim/ClientApp/src/components/Login.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { Button, Col, Form, Input, Row } from 'reactstrap';
+import { Button, Col, Form, FormFeedback, Input, Row, Toast, ToastBody, ToastHeader } from 'reactstrap';
 import FooterImage from '../assets/footer.svg';
 import { ApplicationState } from '../store';
 import * as WeatherForecastsStore from '../store/Employee';
@@ -19,8 +19,20 @@ class Login extends React.PureComponent<WeatherForecastProps> {
       Email: "",
       Password: "",
       Error: false
-      };
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps: any, nextState: any) {
+    const { Email, Password } = this.state as any;
+
+    if ((Email !== nextState.Email || Password !== nextState.Password) && nextState.Error) {
+      this.setState({ Error: false });
+
+      return false;
+    }
+
+    return true;
   }
 
   componentDidMount() {
@@ -31,7 +43,7 @@ class Login extends React.PureComponent<WeatherForecastProps> {
     event.preventDefault();
 
     const state: any = this.state;
-      const { Email, Password } = state;
+    const { Email, Password } = state;
 
     if (Email === "admin@admin.com" && Password === "admin") {
       sessionSet("isLogged", "true", 15);
@@ -44,38 +56,41 @@ class Login extends React.PureComponent<WeatherForecastProps> {
 
   public render() {
     const state: any = this.state;
-    const { Email } = state;
+    const { Email, Error } = state;
 
     return (
       <React.Fragment>
         <div className="app-login">
-        <Form onSubmit={(e) => this.handleSubmit(e)}>
-          <Row>
-            <Col>
-              <h1>Logo empresa</h1>
-            </Col>
-          </Row>
-          <Row className='mt-3'>
-            <Col>
-              <p>Realize seu login para acessar as funcionalidades</p>
-            </Col>
-          </Row>
-          <Row className='mt-3 justify-content-center'>
-            <Col xs={5}>
-              <Input type="email" value={Email} placeholder='Usuário' onChange={(e) => this.setState({ Email: e.target.value })} />
-            </Col>
-          </Row>
-          <Row className='mt-3 justify-content-center'>
-            <Col xs={5}>
-              <Input type="password" placeholder='Senha' onChange={(e) => this.setState({ Password: e.target.value })} />
-            </Col>
-          </Row>
-          <Row className='mt-5 justify-content-center'>
-            <Col xs={5}>
-              <Button type="submit">Login</Button>
-            </Col>
-          </Row>
-        </Form>
+          <Form onSubmit={(e) => this.handleSubmit(e)}>
+            <Row>
+              <Col>
+                <h1>Logo empresa</h1>
+              </Col>
+            </Row>
+            <Row className='mt-3'>
+              <Col>
+                <p>Realize seu login para acessar as funcionalidades</p>
+              </Col>
+            </Row>
+            <Row className='mt-3 justify-content-center'>
+              <Col xs={5}>
+                <Input type="email" value={Email} placeholder='Usuário' invalid={Error} onChange={(e) => this.setState({ Email: e.target.value })} />
+              </Col>
+            </Row>
+            <Row className='mt-3 justify-content-center'>
+              <Col xs={5}>
+                <Input type="password" placeholder='Senha' invalid={Error} onChange={(e) => this.setState({ Password: e.target.value })} />
+              </Col>
+              <FormFeedback>
+                Cpf inválido
+              </FormFeedback>
+            </Row>
+            <Row className='mt-5 justify-content-center'>
+              <Col xs={5}>
+                <Button type="submit">Login</Button>
+              </Col>
+            </Row>
+          </Form>
         </div>
         <footer>
           <img src={FooterImage} alt="Footer Image" />
